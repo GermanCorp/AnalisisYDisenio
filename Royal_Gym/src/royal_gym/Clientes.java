3bfd80a7082af4fb37fb81ed39843d31ea98e6c7
GermanCorp <36717136+GermanCorp@users.noreply.github.com>
2018-03-26 17:55:50 -0600

    German

--- Royal_Gym/src/royal_gym/Clientes.java
+++ Royal_Gym/src/royal_gym/Clientes.java
@@ -2,6 +2,7 @@ package royal_gym;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,6 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import static royal_gym.Conexion.resultado;
 
 
 public class Clientes {
@@ -20,14 +22,9 @@ public class Clientes {
     static ResultSet resultado;
     
     
-    //Columnas de la tabla Clientes
-    private final String[] columnasClientes = {
-        "No.", "Nombres", "Fecha de Nacimiento", "Altura", "Peso", "Masa Corporal", "Clasificación"
-    };
-
     // Modelo de la tabla de Clientes
-    public void modeloTablaCliente(JTable tabla) {
-        DefaultTableModel modeloTablaClientes = new DefaultTableModel(getCliente(), columnasClientes);
+    public void modeloTablaCliente(String[] columnas, JTable tabla) {
+        DefaultTableModel modeloTablaClientes = new DefaultTableModel(getCliente(), columnas);
         tabla.setModel(modeloTablaClientes);
     }
 
@@ -94,5 +91,56 @@ public class Clientes {
         DecimalFormat df = new DecimalFormat("#,##0.00");
         return df.format(numero);
     }// </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Método para insertar un cliente">  
+    public void insertarCliente(String nombres, String apellidos, String fechaNacimiento, String altura, String peso) {
+        try {
+            String sql = "insert into cliente (nombres, apellidos, fechaNacimiento, altura, peso) values (?,?,?,?,?)";
+            PreparedStatement consulta = Conexion.getConexion().prepareStatement(sql);
+            consulta.setString(1, nombres);
+            consulta.setString(2, apellidos);
+            consulta.setString(3, fechaNacimiento);
+            consulta.setString(4, altura);
+            consulta.setString(5, peso);
+            consulta.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }// </editor-fold> 
+    
+    // <editor-fold defaultstate="collapsed" desc="Método para buscar un clientes">  
+    public Object[][] buscarCliente(String nombres, String apellidos) {
+        Object[][] datosCliente = null;
+        try {
+            String consulta = "SELECT nombres ||' '||apellidos as NombreCompleto, fechaNacimiento, altura, peso, Peso / (Altura * Altura) as imc FROM cliente WHERE nombres ||' '||apellidos LIKE '%' || ? || '%' ORDER BY nombres";
+            PreparedStatement statement = Conexion.getConexion().prepareStatement(consulta);
+            statement.setString(1, nombres);
+            //statement.setString(2, apellidos);
 
+            resultado = statement.executeQuery();
+            int numeroLista = 1;
+            DecimalFormat df = new DecimalFormat("0.00");
+
+            ArrayList<Object[]> filas = new ArrayList<>();
+            while (resultado.next()) {
+                filas.add(
+                        new Object[]{
+                            numeroLista++,
+                            resultado.getString("NombreCompleto"),
+                            resultado.getString("FechaNacimiento"),
+                            resultado.getString("Altura"),
+                            resultado.getString("Peso"),
+                            df.format(resultado.getDouble("imc")),
+                            clasificaciónIMC(resultado.getDouble("imc"))
+                        }
+                );
+            }
+            datosCliente = new Object[filas.size()][];
+            filas.toArray(datosCliente);
+        } catch (Exception e) {
+            System.out.println("buscarCliente:" + e.getMessage());
+        }
+        return datosCliente;
+    }// </editor-fold>
+    
 }
