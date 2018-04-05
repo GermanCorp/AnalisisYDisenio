87d2ffde74d40ae662dd33a8a30f766d6d050e10
GermanCorp <36717136+GermanCorp@users.noreply.github.com>
2018-04-04 22:24:43 -0600

    lista de clientes

--- Royal_Gym/src/presentacion/PanelListaDeClientes.java
+++ Royal_Gym/src/presentacion/PanelListaDeClientes.java
@@ -5,35 +5,36 @@
  */
 package presentacion;
 
+import javax.swing.JDialog;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import royal_gym.Conexion;
 import royal_gym.VentanaPrincipal;
 
-/**
- *
- * @author alxcr
- */
-
 public class PanelListaDeClientes extends javax.swing.JFrame {
     private final Conexion con;
-    VentanaPrincipal ventanap = new VentanaPrincipal();
-    
+    PanelVentas pventas;
     private final String[] columnasClientes = {
         "No.",
         "Nombre"
     };
     
     public PanelListaDeClientes() {
+        
         initComponents();
-        setTitle("Clientes");
+        //pventas = new PanelVentas();
+        //setDefaultCloseOperation(HIDE_ON_CLOSE);
+        //setTitle("Clientes");
         this.con = new Conexion();
-        con.conectar();
-        setLocationRelativeTo(null);
+        //setLocationRelativeTo(null);
         
         DefaultTableModel modeloTablaClientes = new DefaultTableModel(con.getCliente(), columnasClientes);
         tablaClientes.setModel(modeloTablaClientes);
-        
+
     }
+    
+    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -41,6 +42,7 @@ public class PanelListaDeClientes extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaClientes = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,8 +61,18 @@ public class PanelListaDeClientes extends javax.swing.JFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 tablaClientesMouseClicked(evt);
             }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                tablaClientesMousePressed(evt);
+            }
         });
         jScrollPane1.setViewportView(tablaClientes);
+
+        jButton1.setText("Aceptar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -68,26 +80,45 @@ public class PanelListaDeClientes extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(jScrollPane1)
-                .addGap(20, 20, 20))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1)
+                        .addGap(20, 20, 20))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(jScrollPane1)
-                .addGap(20, 20, 20))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(jButton1)
+                .addContainerGap(35, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void tablaClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaClientesMouseClicked
-
+        int filaSeleccionada = tablaClientes.getSelectedRow();
+            String nombreCliente = tablaClientes.getValueAt(filaSeleccionada, 1).toString();
+            pventas.jtfNombre.setText(nombreCliente);
     }//GEN-LAST:event_tablaClientesMouseClicked
+
+    private void tablaClientesMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaClientesMousePressed
+        // TODO add your handling code here:     
+        
+    }//GEN-LAST:event_tablaClientesMousePressed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tablaClientes;
     // End of variables declaration//GEN-END:variables
